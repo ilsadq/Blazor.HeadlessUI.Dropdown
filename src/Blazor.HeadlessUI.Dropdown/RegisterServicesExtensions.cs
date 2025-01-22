bbdd5dfc9f7a802f4515ccf2640d062b21c876f7
adequate <tenoba9@gmail.com>
2025-01-22 14:17:10 +0200

    Init commit

---
+++ src/Blazor.HeadlessUI.Dropdown/RegisterServicesExtensions.cs
@@ -0,0 +1,14 @@
+using Blazor.FloatingUI;
+using Blazor.FocusTrapJs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blazor.HeadlessUI.Dropdown;
+
+public static class DependencyInjectionExtensions
+{
+    public static void AddDropdownServices(this IServiceCollection services)
+    {
+        services.AddScoped<FloatingJsProvider>();
+        services.AddScoped<FocusTrapJsProvider>();
+    }
+}
